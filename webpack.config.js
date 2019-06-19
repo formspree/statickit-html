38539180f16674612fe7ba1ca3b6c4663cf9405c
Derrick Reimer <derrickreimer@gmail.com>
2019-06-19 10:29:12 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'statickit.js',
+    publicPath: '/' // used by webpack-dev-middleware
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  }
+};
