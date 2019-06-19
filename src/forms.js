b51e0f44f0452e5698786ebebf7a664af16e40d7
Derrick Reimer <derrickreimer@gmail.com>
2019-06-19 12:35:00 -0500

    Add some documentation

--- src/forms.js
+++ src/forms.js
@@ -1,15 +1,24 @@
+/**
+ * Gets all `<form>` elements that are under StaticKit control.
+ */
 const getForms = document => {
   return Array.from(document.forms).filter(el => {
     return el.dataset.skId !== undefined;
   });
 };
 
+/**
+ * Disables all submit buttons in a form.
+ */
 const disableSubmitButton = formEl => {
   Array.from(formEl.querySelectorAll("[type='submit']")).forEach(buttonEl => {
     buttonEl.disabled = true;
   });
 };
 
+/**
+ * Hijacks form submission.
+ */
 const setupForm = formEl => {
   formEl.addEventListener("submit", ev => {
     ev.preventDefault();
