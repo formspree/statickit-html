1abbfcd545d84f3be680a384497739eade7f19fb
Derrick Reimer <derrickreimer@gmail.com>
2019-06-19 11:06:47 -0500

    Add ready function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Hello world!");
+const ready = require('./ready');
+
+ready(() => {
+  console.log("Hello world")
+});
---
+++ src/ready.js
@@ -0,0 +1,12 @@
+module.exports = (fn) => {
+  if (document.readyState != 'loading'){
+    fn();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    document.attachEvent('onreadystatechange', function() {
+      if (document.readyState != 'loading')
+        fn();
+    });
+  }
+};
